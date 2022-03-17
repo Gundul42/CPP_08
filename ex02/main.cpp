83ad9ea2d2328ac6e6efc6d75f6a1ddfb1081090
Gundul <sampah@mailbox.org>
2022-03-17 14:23:13 +0100

    add more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 18:22:22 by graja             #+#    #+#             */
-/*   Updated: 2022/03/17 11:19:48 by graja            ###   ########.fr       */
+/*   Updated: 2022/03/17 14:22:16 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	main(void)
 {
-	//changing comments to try ith STL stack
+	//changing comments to try with STL stack
 	MutantStack<int>	test;
 	//std::stack<int>		test;
 	
@@ -36,7 +36,7 @@ int	main(void)
 		std::cout << "not ";
 	std::cout << "empty" << std::endl;
 
-	//changing comments to try ith STL stack
+	//changing comments to try with STL stack
 	MutantStack<int>::iterator	itt = test.begin();
 	//std::stack<int>::iterator	itt = test.begin();
 	
@@ -44,6 +44,66 @@ int	main(void)
 	{
 		std::cout << *itt << ", ";
 		itt++;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	//TEST WITH FLOATS
+	MutantStack<float>	test1;
+	
+	test1.push(10);
+	test1.push(-3);
+	test1.push(12.10);
+	test1.push(3912.81);
+	test1.push(-12.897);
+	test1.push(66.6);
+
+	std::cout << "Member function size: " << test1.size() << std::endl;
+	test1.push(-33.3);
+	std::cout << "Member function top: " << test1.top() << std::endl;
+	test1.pop();
+	std::cout << "Member function pop: " << test1.top() << std::endl;
+	std::cout << "Test is "; 
+	if (!test1.empty())
+		std::cout << "not ";
+	std::cout << "empty" << std::endl;
+
+	MutantStack<float>::iterator	itt1 = test1.begin();
+	
+	while (itt1 != test1.end())
+	{
+		std::cout << *itt1 << ", ";
+		itt1++;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	//TEST WITH STRING
+	MutantStack<std::string>	test2;
+	
+	test2.push("lala");
+	test2.push("lolo");
+	test2.push("42");
+	test2.push("Wolfsburg");
+	test2.push("Himmelbett");
+	test2.push("Kuemmelstange");
+
+	std::cout << "Member function size: " << test2.size() << std::endl;
+	test2.push("No Entry");
+	std::cout << "Member function top: " << test2.top() << std::endl;
+	test2.pop();
+	std::cout << "Member function pop: " << test2.top() << std::endl;
+	std::cout << "Test is "; 
+	if (!test2.empty())
+		std::cout << "not ";
+	std::cout << "empty" << std::endl;
+
+	MutantStack<std::string>::iterator	itt2 = test2.begin();
+	
+	while (itt2 != test2.end())
+	{
+		std::cout << *itt2 << ", ";
+		itt2++;
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
